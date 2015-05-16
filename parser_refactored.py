8e4f551eb9c82d97a4a26429efc773aaa7e4317a
robson <robsonf@gmail.com>
2015-05-16 18:51:07 -0300

    tratamento da saida

--- parser_refactored.py
+++ parser_refactored.py
@@ -17,15 +17,15 @@ def export_cnf(variables, clauses):
     cnf_file.close()
 
 def run_solver():
-    os.system("./zchaff64/zchaff cnf > result.txt")
+    os.system("./zchaff64/zchaff cnf > result_solver.txt")
     #os.system("rm cnf")
 
 def is_solution():
-    output_solver = open("result.txt").read()
+    output_solver = open("result_solver.txt").read()
     return "RESULT:	SAT" in output_solver
 
 def extract_plan(variables, action_fluents):
-    iter_solver = iter(open("result.txt").readlines())
+    iter_solver = iter(open("result_solver.txt").readlines())
     while iter_solver.next() != "Instance Satisfiable\n":
         continue 
     aux = iter_solver.next().split()
@@ -46,6 +46,19 @@ def extract_plan(variables, action_fluents):
 #    aux.close()
     return plan
 
+def output_list(l):
+    output_file = open('output_list.txt','a')
+    output_file.write(str(l) + '\n')
+    output_file.close()
+
+def output_lattex(l):
+    output_file = open('output_lattex.txt','a')
+    output_file.write(l[0] + ' & ' + str(l[1]) + ' & ' + str(l[2]) + ' & ' + str(l[3]) + ' & ' + "%.2f" % round(l[4],2) + ' \\\\ \n')
+    output_file.close()
+    
+def output_chart():
+    return
+
 '''
 TODO: ESTATÍSTICAS
 plano com tabela de símbolos,
@@ -57,22 +70,30 @@ tempo para encontrar o plano (incluindo o tempo de geração da CNF).
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i','--input', help='foo help', default = 'cake.strips')
+    #parser.add_argument('-i','--input', help='help', default = 'cake.strips')
+    parser.add_argument('-f','--input', help='help', default = 'domains/Blocks-Ground/')
     args = parser.parse_args()
-
-    initial_time = clock()
-    #problem = Problem.read_strips(file_name)
-
-    problem = Problem.read_strips(args.input)
-    time = 0
-    while(True):
-        time = time + 1
-        variables, clauses = clauses_extractor(time, problem)
-        export_cnf(variables, clauses)
-        run_solver()
-        if is_solution():
-            plan = extract_plan(variables, problem.action_fluents)
-            break
-    return plan, len(variables), len(clauses), len(plan), clock()-initial_time
-
-print main()
+    files_input = open(args.input+'files_input').read().split('\n')
+    files_input = files_input[:len(files_input)-1]
+    output_lines = []
+    #print [file_name for file_name in files_input]
+    for file_name in files_input:
+        initial_time = clock()
+        problem = Problem.read_strips(args.input+file_name)
+        #problem = Problem.read_strips(args.input)
+        time = 0
+        while(True):
+            time = time + 1
+            variables, clauses = clauses_extractor(time, problem)
+            export_cnf(variables, clauses)
+            run_solver()
+            if is_solution():
+                plan = extract_plan(variables, problem.action_fluents)
+                break
+        output_line = [file_name] + [len(variables)] + [len(clauses)] + [len(plan)] + [clock()-initial_time] + [plan]
+        output_list(output_line)
+        output_lattex(output_line)
+        #output_lines.append([file_name] + [len(variables)] + [len(clauses)] + [len(plan)] + [clock()-initial_time] + [plan])
+    #return output_lines
+    
+main()
